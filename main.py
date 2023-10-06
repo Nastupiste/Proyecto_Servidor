683ae9029ce374be42f146a8cf1677826f4a612c
Manuel <mjroscos@gmail.com>
2023-10-06 18:34:00 +0200

    pruebas git

--- main.py
+++ main.py
@@ -12,7 +12,23 @@ from tkinter import *
 respuesta=requests.get("https://www.bne.es/sites/default/files/redBNE/datosgob/awe/tema/videojuegos.json")
 datos=respuesta.json()
 
+# Creamos una nueva ventana:
+# Convertimos a texto los datos:
+texto = json.dumps(datos, ensure_ascii= False, indent=2)
+ventana = tk.Tk()
 
+# Añado un elemento scrollBar a la ventana:
+scrol_bar= tk.Scrollbar(ventana)
+scrol_bar.pack(side='right', fill='y')
+
+# Para poder configurar el texto y formato de la ventana
+txt = tk.Text(ventana, font="Times32")
+txt.pack()
+txt.config(yscrollcommand=scrol_bar.set)
+scrol_bar.config(command=txt.yview)
+txt.insert('end', texto)
+
+ventana.mainloop()
 
 
 # Si quisiera imprimir los datos obtenidos en formato json:
