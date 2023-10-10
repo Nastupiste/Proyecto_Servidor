24684396ac3540dc5b2686b5eb11a39249b30ce4
Manuel <mjroscos@gmail.com>
2023-10-11 00:09:03 +0200

    incorporo, window, labels y entries

--- main.py
+++ main.py
@@ -5,40 +5,53 @@ import requests
 # Para generar un número aleatorio
 import random
 # Para poder tener ventana.
-import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
-#Funciones
+numPreguntas=""
+
+# Funciones
 def respuestaValida(opciones):
-    r_incorrecta=True
-    while r_incorrecta:
+    while True:
         try:
-            respuesta=int(input("Enter the number of your answer: "))
+            respuesta=int(entry.get())
             if 1<=respuesta<=opciones:
-                r_incorrecta=False
                 return respuesta
             else:
+
                 print("Invalid answer. Please choose a number between 1 and", opciones)
         except ValueError:
             print("Sorry, you should write a valid answer: ")
 
 def validarNumPreguntas():
-    r_incorrecta=True
-    while r_incorrecta:
+    while True:
         try:
-            respuesta=int(input("How many questions do yo want to answer?\n(from 1 to 50):"))
+            respuesta=int(entry.get())
+            numPreguntas=respuesta
             if 1<=respuesta<=50:
-                r_incorrecta=False
                 return respuesta
             else:
-                print("Invalid answer. Please choose a number between 1 and 50.")
+                label = ttk.Label(root_window, text="Invalid answer. Please choose a number between 1 and 50.")
         except ValueError:
-            print("Sorry, you should enter a number between 1 and 50. ")
+            label = ttk.Label(root_window, text="Sorry, you should enter a number between 1 and 50.")
+
+# Creo ventana principal
+
+root_window=Tk()
+root_window.minsize(width=500, height=500)
+root_window.title("Welcome to the Trivia´s API quest!")
 
 # Interacción con el usuario
-print()
-print("Welcome to the Trivia´s API quest!")     
-numPreguntas=validarNumPreguntas()
+label = ttk.Label(root_window, text="How many questions do yo want to answer?\n(from 1 to 50):")
+label.pack()
+# Recogida de datos
+
+entry=ttk.Entry()
+entry.pack()
+submit_button = ttk.Button(root_window, text="Enviar", command=validarNumPreguntas)
+submit_button.pack()
+
+print("Preguntas: "+numPreguntas)
 
 # Hacemos la solicitud de los datos a la url.
 categoria=int(random.randint(9,30))
@@ -65,13 +78,18 @@ preguntas=datos["results"]
 fallos=0
 aciertos=0
 # En la cada vuelta accedemos a una pregunta que contienen respuestas buenas y malas
-print()
-print("The category is "+preguntas[0]["category"])
-for pregunta in preguntas:
+
+label = ttk.Label(root_window, text="The category is "+preguntas[0]["category"])
+
+"""for pregunta in preguntas:
     opciones=[]
-    print()
-    print(pregunta["question"])
-    print("Options: ")
+    
+    label_question=ttk.Label(root_window, text=pregunta["question"])
+    label_question.pack()
+
+    option_title=ttk.Label(root_window, text="Options: ")
+    option_title.pack()
+
     # Este for añade las respuestas malas a una lista de opciones
     for numRespuesta in range(len(pregunta["incorrect_answers"])): 
        opciones.append(pregunta["incorrect_answers"][numRespuesta])
@@ -83,58 +101,41 @@ for pregunta in preguntas:
    
     # imprimimos la respuesta por pantalla. ¡Hasta aquí genial! 
     contador=0
+    cadena_respuestas=""
+
     for respuesta in range(len(opciones)):
         contador=contador+1
-        print(f"{contador} {opciones[respuesta]}")
+        cadena_respuestas+=(f"{contador} {opciones[respuesta]}\n")
+
+    label_answers=ttk.Label(root_window, text=cadena_respuestas)
+    label_answers.pack()
 
     # recogemos la respuesta del usuario y realizo la comprobación
     # prevengo que la respuesta no sea un carácter o un número mayor que las opciones disponibles.
     respuestaUsuario=-1
+
+
+    label = ttk.Label(root_window, text="Enter your answer: ")
+
     respuestaUsuario=respuestaValida(len(opciones))
+    
+    # Necesitamos restar 1 a la respuesta, porque la lista va de 0 a 3 como máximo.
     respuestaUsuario-=1
     if opciones[respuestaUsuario]==pregunta["correct_answer"]:
         aciertos+=1
     else:
         fallos+=1
 
-porcentaje_Acierto = (aciertos/numPreguntas) * 100
-print()
+#porcentaje_Acierto = (aciertos/numPreguntas) * 100
+
 if porcentaje_Acierto>=50:
-    print("Well done! You have passed the quiz!")
+    final_label=ttk.Label(root_window, text="Well done! You have passed the quiz!")
+    final_label.pack()
 else:
-    print("You lose... Don't worry, you'll get them next time!")
-print()
+    final_label=ttk.Label(root_window, text="You lose... Don't worry, you'll get them next time!")
+    final_label.pack()
+
 print(f"You had {aciertos} correct answers.")
 print(f"You had {fallos} incorrect answers.")
-print(f"Your final score is {porcentaje_Acierto}")
-
-
-
-
-
-
-
-
-
-
-
-# Creamos una nueva ventana:
-# Convertimos a texto los datos:
-#texto = json.dumps(datos, ensure_ascii= False, indent=2)
-#ventana = tk.Tk()
-
-# Añado un elemento scrollBar a la ventana:
-#scrol_bar= tk.Scrollbar(ventana)
-#scrol_bar.pack(side='right', fill='y')
-
-# Para poder configurar el texto y formato de la ventana
-#txt = tk.Text(ventana, font="Times32")
-#txt.pack()
-#txt.config(yscrollcommand=scrol_bar.set)
-#scrol_bar.config(command=txt.yview)
-#txt.insert('end', texto)
-
-#ventana.mainloop()
-
-# Si quisiera imprimir los datos obtenidos en formato json:
-# print(json.dumps(datos, indent=4, sort_keys=True))*/
+print(f"Your final score is {porcentaje_Acierto}")"""
+root_window.mainloop()
--- pruebaGUI.py
+++ pruebaGUI.py
@@ -1,10 +1,30 @@
 
 from tkinter import *
 from tkinter import ttk
-root = Tk()
-root.minsize(width=500, height=500)
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+
+def validarNumPreguntas():
+    while True:
+        try:
+            respuesta=int(entry.get())
+            if 1<=respuesta<=50:
+                return respuesta
+            else:
+                label = ttk.Label(root_window, text="Invalid answer. Please choose a number between 1 and 50.")
+        except ValueError:
+            label = ttk.Label(root_window, text="Sorry, you should enter a number between 1 and 50.")
+
+
+root_window=Tk()
+root_window.minsize(width=500, height=500)
+root_window.title("Welcome to the Trivia´s API quest!")
+
+# Interacción con el usuario
+label = ttk.Label(root_window, text="How many questions do yo want to answer?\n(from 1 to 50):")
+label.pack()
+# Recogida de datos
+entry=ttk.Entry()
+entry.pack()
+submit_button = ttk.Button(root_window, text="Enviar", command=validarNumPreguntas)
+submit_button.pack()
+#numPreguntas=validarNumPreguntas(entry)
+root_window.mainloop()
