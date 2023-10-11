5f6e8f5658a4b9a85318e58bf5ea6035b337a5d3
Nastupiste <mjroscos@gmail.com>
2023-10-11 10:13:49 +0200

    reestructuración de archivos

--- pruebaGUI.py
+++
@@ -1,8 +0,0 @@
-
-import pyQt
-
-QtGui.QMessageBox.question(self, 
-         'Message',
-         "Are you sure to quit?",
-         QtGui.QMessageBox.Yes, 
-         QtGui.QMessageBox.No)
--- ventana_ui.py
+++ ventana_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Trivia´s_window.ui'
+# Form implementation generated from reading ui file 'ventana.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
